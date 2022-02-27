0fda90f6c556cd9608d5d7ac68ada4b1ecf72e58
sendkite <sendkite1@gmail.com>
2022-02-27 23:45:52 +0900

    create : PRG로 중복 post 해결

--- src/main/java/com/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/com/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -39,9 +40,11 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String save(@ModelAttribute Item item) {
-        itemRepository.save(item);
-        return "basic/item";
+    public String save(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
